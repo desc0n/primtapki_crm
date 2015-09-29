14ac2d609cc7905b1a7d57fc750fcc47d94dbd50
desc0n <desconbk@yandex.ru>
2015-09-29 23:36:29 +1000

    Меняю пути к файлам стилей в шаблонах

--- application/views/login.php
+++ application/views/login.php
@@ -12,16 +12,16 @@
     <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/crm/public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="/public/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="/crm/public/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/public/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="/crm/public/dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/crm/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -61,16 +61,16 @@
 </div>
 
 <!-- jQuery -->
-<script src="/public/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/crm/public/bower_components/jquery/dist/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/crm/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="/public/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+<script src="/crm/public/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="/public/dist/js/sb-admin-2.js"></script>
+<script src="/crm/public/dist/js/sb-admin-2.js"></script>
 
 </body>
 
--- application/views/template.php
+++ application/views/template.php
@@ -11,22 +11,22 @@
     <title>CRM</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/crm/public/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="/public/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="/crm/public/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
     <!-- Timeline CSS -->
-    <link href="/public/dist/css/timeline.css" rel="stylesheet">
+    <link href="/crm/public/dist/css/timeline.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/public/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="/crm/public/dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="/public/bower_components/morrisjs/morris.css" rel="stylesheet">
+    <link href="/crm/public/bower_components/morrisjs/morris.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/crm/public/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -709,21 +709,21 @@
 <!-- /#wrapper -->
 
 <!-- jQuery -->
-<script src="/public/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/crm/public/bower_components/jquery/dist/jquery.min.js"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/crm/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 <!-- Metis Menu Plugin JavaScript -->
-<script src="/public/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+<script src="/crm/public/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
 <!-- Morris Charts JavaScript -->
-<script src="/public/bower_components/raphael/raphael-min.js"></script>
-<script src="/public/bower_components/morrisjs/morris.min.js"></script>
-<script src="/public/js/morris-data.js"></script>
+<script src="/crm/public/bower_components/raphael/raphael-min.js"></script>
+<script src="/crm/public/bower_components/morrisjs/morris.min.js"></script>
+<script src="/crm/public/js/morris-data.js"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="/public/dist/js/sb-admin-2.js"></script>
+<script src="/crm/public/dist/js/sb-admin-2.js"></script>
 
 </body>
 
